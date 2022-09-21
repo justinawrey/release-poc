fe421a9c6c68824e0d31d4ea73a4b2ded5983048
Justin Awrey <awreyjustin@gmail.com>
2022-09-21 10:00:02 -0700

    feat!: use conventional commits format

--- packages/release-mfe/src/index.ts
+++ packages/release-mfe/src/index.ts
@@ -20,8 +20,11 @@ const config: Options = {
     },
   ],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/release-notes-generator",
+      { preset: "conventionalcommits" },
+    ],
     "@semantic-release/changelog",
     ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
