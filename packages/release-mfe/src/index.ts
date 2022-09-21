5eac1a8ca5331e17cb3c5c4a3be7fdd28e183194
Justin Awrey <awreyjustin@gmail.com>
2022-09-21 09:47:06 -0700

    try main -> latest

--- packages/release-mfe/src/index.ts
+++ packages/release-mfe/src/index.ts
@@ -13,21 +13,26 @@ if (!packageJson) {
 
 const config: Options = {
   extends: "semantic-release-monorepo",
-  branches: ["main"],
+  branches: [
+    {
+      name: "main",
+      channel: "latest",
+    },
+  ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message:
-          "chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["package.json", "CHANGELOG.md"],
+    //     message:
+    //       "chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}",
+    //   },
+    // ],
   ],
   tagFormat: `${packageJson.default.name}-v\${version}`,
 };
