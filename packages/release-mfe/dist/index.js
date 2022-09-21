366e87553ce526da666eb1e765369e8519ce9327
Justin Awrey <awreyjustin@gmail.com>
2022-09-21 10:48:00 -0700

    new dist

--- packages/release-mfe/dist/index.js
+++ packages/release-mfe/dist/index.js
@@ -6,20 +6,29 @@ if (!packageJson) {
 }
 const config = {
     extends: "semantic-release-monorepo",
-    branches: ["main"],
+    branches: [
+        {
+            name: "main",
+            channel: "latest",
+        },
+    ],
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
+        ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+        [
+            "@semantic-release/release-notes-generator",
+            { preset: "conventionalcommits" },
+        ],
         "@semantic-release/changelog",
         ["@semantic-release/npm", { npmPublish: false }],
         "@semantic-release/github",
-        [
-            "@semantic-release/git",
-            {
-                assets: ["package.json", "CHANGELOG.md"],
-                message: "chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}",
-            },
-        ],
+        // [
+        //   "@semantic-release/git",
+        //   {
+        //     assets: ["package.json", "CHANGELOG.md"],
+        //     message:
+        //       "chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}",
+        //   },
+        // ],
     ],
     tagFormat: `${packageJson.default.name}-v\${version}`,
 };
@@ -42,3 +51,4 @@ try {
 catch (err) {
     console.error("The automated release failed with %O", err);
 }
+console.log("done");
